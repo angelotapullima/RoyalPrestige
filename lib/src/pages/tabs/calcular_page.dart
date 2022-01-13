98ff7821c83f144016e9f24936b3601ff44f9615
cjunior20 <christianzavaleta20@gmail.com>
2022-01-13 14:05:37 -0500

    calcular 50%

--- lib/src/pages/tabs/calcular_page.dart
+++ lib/src/pages/tabs/calcular_page.dart
@@ -1,10 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:royal_prestige/src/utils/colors.dart';
 
-class CalcularPage extends StatelessWidget {
+class CalcularPage extends StatefulWidget {
   const CalcularPage({Key? key}) : super(key: key);
 
   @override
+  State<CalcularPage> createState() => _CalcularPageState();
+}
+
+class _CalcularPageState extends State<CalcularPage> {
+  final TextEditingController _precioProductoController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(
+          top: kBottomNavigationBarHeight + ScreenUtil().setHeight(10),
+          left: ScreenUtil().setWidth(21),
+          right: ScreenUtil().setWidth(21),
+        ),
+        child: Column(
+          children: [
+            Container(
+              child: TextField(
+                maxLines: 1,
+                controller: _precioProductoController,
+                keyboardType: TextInputType.number,
+                obscureText: true,
+                decoration: InputDecoration(
+                  filled: true,
+                  labelText: 'Precio producto',
+                  labelStyle: TextStyle(
+                    color: colorgray,
+                    fontWeight: FontWeight.w400,
+                    fontSize: ScreenUtil().setSp(12),
+                  ),
+                  fillColor: Colors.white,
+                  contentPadding: EdgeInsets.only(left: ScreenUtil().setWidth(10), top: ScreenUtil().setHeight(5), bottom: ScreenUtil().setHeight(1)),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: colorPrimary, width: ScreenUtil().setWidth(1)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: colorPrimary, width: ScreenUtil().setWidth(1)),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: colorPrimary, width: ScreenUtil().setWidth(1)),
+                  ),
+                ),
+                style: TextStyle(
+                  color: colorPrimary,
+                  fontWeight: FontWeight.w700,
+                  fontSize: ScreenUtil().setSp(15),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+}
+
+class ControllerCalculo extends ChangeNotifier {
+  double fivepercent = 0, tenpercent = 0, fifteenpercent = 0;
 }
--- lib/src/utils/colors.dart
+++ lib/src/utils/colors.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const Color colorPrimary = Color(0XFF2A4367);
 const Color colorgray = Color(0XFF757575);
 const Color colorOff = Color(0XFFAFAFAF);
+const Color colorOffText = Color(0XFFC4C4C4);
+const Color colorBack = Color(0XFFE5E5E5);
